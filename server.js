851e5812a8e526a5ea324c92752974cef794ddab
Aman <hiamanagrawal@gmail.com>
2024-03-03 10:52:17 +0530

    added mongodb client code successfully

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const mongoose = require('mongoose');
+require("dotenv").config();
 const app = express();
 const port = 8081;
 
@@ -9,4 +10,16 @@ app.get('/ping' , (req , res)=>{
 
 app.listen(port,()=>{
     console.log(`server is running on ${port}`)
+})
+
+app.get('/home',(req,res)=>{
+    mongoose.connect(process.env.URI)
+    .then(()=>{
+        console.log("connected to mongoDB")
+    })
+    .catch((err)=>{
+        console.error(err)
+    })
+    const dbStatus = mongoose.connection ? "connected" : "Disconnected";
+    res.send(`Database Connection Status: ${dbStatus}`);
 })
